a0196ec4b6d34a4f163b47811ad10b26b578f912
Jakob Kohler <schasch.dev@protonmail.com>
2023-03-07 12:17:20 +0100

    Fixed dates being shifted in the current week

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(cors());
 const port = process.env.NODE_PORT || 3001;
 
 app.get('/', async (req, res) => {
+    const dateProvided = !!req.query.date;
     const today = new Date();
     const date = req.query.date ? req.query.date : 
         `${today.getFullYear()}-${("0" + (today.getMonth() + 1)).slice(-2)}-${("0" + today.getDate()).slice(-2)}`;
@@ -17,10 +18,13 @@ app.get('/', async (req, res) => {
         await fetchMensa('simplesite', { canteens: ['moltke'], dates: [date] })
         .then(data => {
             let weekDay = new Date(date).getDay();
+
             if(weekDay == 0 || weekDay == 6) return res.send([]);
 
-            if(data[weekDay - 1]){
-                res.send(data[weekDay - 1]);
+            let dataIndex = dateProvided ? calculateDataIndex(req.query.date) : 0;
+            
+            if(data[dataIndex]){
+                res.send(data[dataIndex]);
             }else{
                 res.send([])
             }
@@ -34,14 +38,33 @@ app.get('/koeriStatus', async (req, res) => {
             let weekDay = date.getDay();
             if(weekDay == 0 || weekDay == 6 || (data.length == 0)) return res.send(JSON.stringify({"koeriOpen" : false}));
 
-            if(data[weekDay - 1]){
-                let koeriStatus = data[weekDay - 1].lines[6].meals.length === 0 && data[weekDay - 1].lines[2].meals.length === 0;
-                return res.send(JSON.stringify({"koeriOpen" : false}))
+            if(data[0]){
+                let koeriStatus = data[0].lines[6].meals.length === 0 && data[0].lines[2].meals.length === 0;
+                return res.send(JSON.stringify({"koeriOpen" : !koeriStatus}))
             }
 
-            res.send(JSON.stringify({"koeriOpen" : true}));
+            res.send(JSON.stringify({"koeriOpen" : false}));
         });
 });
 
 
 app.listen(port, () => console.log(`Mensa API Server listening on port ${port}!`));
+
+
+function calculateDataIndex(dateString){
+    const requestedDate = new Date(dateString);
+    const currentDate = new Date();
+    const startDate = new Date(currentDate.getFullYear(), 0, 1);
+
+    let daysOfReqeustedDate = Math.floor((requestedDate - startDate) / (24 * 60 * 60 * 1000));
+    let daysOfCurrentDate = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+
+    const weekNumberRequested = Math.ceil(daysOfReqeustedDate / 7);
+    const weekNumberCurrent = Math.ceil(daysOfCurrentDate / 7);
+
+    if(weekNumberCurrent == weekNumberRequested){
+        return requestedDate.getDay() - currentDate.getDay();
+    }else{
+        return requestedDate.getDay() - 1
+    }
+}
